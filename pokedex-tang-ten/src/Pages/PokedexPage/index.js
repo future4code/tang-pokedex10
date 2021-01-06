e348bff50a315014c85ee940586415cc7cb876ac
tiagosestari <tiagosestari@gmail.com>
2021-01-05 23:08:34 -0300

    Criada a página da pokedex e o componente de Card para ser renderizado nela

---
+++ pokedex-tang-ten/src/Pages/PokedexPage/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {useState} from 'react';
+import CardPokedex from '../../Components/CardPokedex';
+
+const PokedexPage = () => {
+
+    const exemploInicial = [
+        {
+        nome: "Charmander",
+        tipo: "Fogo",
+        imagemURL: "URL da Imagem"
+    },
+    {
+        nome: "Squirtle",
+        tipo: "Água",
+        imagemURL: "URL da Imagem"
+    }
+]
+
+    const [pokemonsSalvos, setPokemonsSalvos] = useState(exemploInicial);
+        
+    return(
+        <div>
+            {pokemonsSalvos.map((pokemon) => {
+                return (
+                    <CardPokedex 
+                        imagem={pokemon.imagemURL}
+                        nome={pokemon.nome}
+                        tipo={pokemon.tipo}
+                        />
+                )
+            }
+            )
+            }
+        </div>
+    )
+}
+
+export default PokedexPage;
